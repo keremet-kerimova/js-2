065aedc46d4669e92d866c3ecf256e276eaadcad
Your Name <you@example.com>
2021-01-15 21:15:54 +0600

    added new  formls

--- script.js
+++ script.js
@@ -1,26 +1,94 @@
-let first = "Shrek";
-let second = first;
-let third = second;
-let fourth = third;
-let fifth = fourth;
-let capital = "Karalol";
-let country = "KG";
-let unit = "meters";
-let subject = "Distance";
-let  book = "Dark Tover";
-let  author = "S.K"
-
-unit = "karakol";
-sabject = " Kerimova";
-unit = "Oruktu";
-subject= "CHon";
-book = "Samurai bez mecha"
-author = "Kitomi"
+// let first = "Shrek";
+// let second = first;
+// let third = second;
+// let fourth = third;
+// let fifth = fourth;
+// let capital = "Karalol";
+// let country = "KG";
+// let unit = "meters";
+// let subject = "Distance";
+// let book = "Dark Tover";
+// let author = "S.K";
+// let artist = "Bin";
+// let movie = "Terminator"
 
 
+
+// let firstName=prompt('What is your name?');
+// let a = +prompt("What is A?");
+// let b = +prompt("What is B?");
+// let sum = (a+b);
+
+//  let c =+prompt("What is C?");
+//  let d =+prompt("What is D?");
+//  let e = c * d;
+
+
+
+// unit = "karakol";
+// sabject = " Kerimova";
+// unit = "Oruktu";
+// subject = "CHon";
+// book = "Samurai bez mecha"
+// author = "Kitomi"
+// artist = "Robert"
+
+
+
+
+// console.log(first);
+// console.log(capital + " is the capital of " + country);
+// console.log(sabject + "is measured in " + unit);
+// console.log("My favourite book is " + book + " by " + author + " !");
+// console.log("My favourite song is   'Yesterday'  by M.K");
+// console.log("I'm the fan of \" " + artist  + " \"!");
+// console.log(`I'm the fan of "Bin"! `);
+// console.log( `I'm  the fan of \"`+ artist + "\"!");
+// console.log (`'My favourite movie is "${movie}"'`);
+
+
+
+
+
+
+// console.log(`Your name is ${firstName}`);
+// console.log(`The sum of two is egual to 10`);
+// console.log(`The sum of two numbers is equal to ${sum}`);
+// console.log(`C multiplied by D is equal to ${e}`);
+
+
+
+
+
+// let  m = +prompt('What is M?');
+// let  g = +prompt('What is G?');
+// let  e = m * g ;
+
+
+// console.log(`G multilated by M is equal to ${e}`);
+
+
+//  let p = +prompt('What is P?');
+//  let p1 = +prompt('What is P1?');
+//  let g = +prompt('What is G?');
+//  let h = +prompt('What is H?');
+//  let u = p+p1*g*h;
   
 
-console.log(first);
-console.log(capital +" is the capital of " + country);
-console.log(sabject + "is measured in " + unit);
-console.log( "My favourite book is " + book +  " by " + author + " !")
+//  console.log(`${u}`);
+
+// let n = +prompt('What is N?');
+// let  m =+prompt('What is M?');
+// let  v =+prompt('What is v?');
+//  let p = 1/3 *n*m*v;
+
+
+//  console.log(`${p}`);
+ 
+
+let q =+prompt('What is Q1?');
+let q2 =+prompt('What is Q2?');
+let q3 =+prompt('What is Q3?');
+let  f = Q1*Q2/Q3*100;
+
+console.log(`${f}`);
