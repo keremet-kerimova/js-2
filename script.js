2a19d95cfaee0e0c4cbb4ffbbc73950750c00bf7
Your Name <you@example.com>
2021-01-15 15:00:09 +0600

    commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let first = "Shrek";
+let second = first;
+let third = second;
+let fourth = third;
+let fifth = fourth;
+let capital = "Karalol";
+let country = "KG";
+let unit = "meters";
+let subject = "Distance";
+let  book = "Dark Tover";
+let  author = "S.K"
+
+unit = "karakol";
+sabject = " Kerimova";
+unit = "Oruktu";
+subject= "CHon";
+book = "Samurai bez mecha"
+author = "Kitomi"
+
+
+  
+
+console.log(first);
+console.log(capital +" is the capital of " + country);
+console.log(sabject + "is measured in " + unit);
+console.log( "My favourite book is " + book +  " by " + author + " !")
