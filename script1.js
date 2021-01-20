fcc50d97fa807a10453d6c7b68122a91e2584f5a
Your Name <you@example.com>
2021-01-20 14:54:34 +0600

    added consol.log

--- script1.js
+++ script1.js
@@ -1,51 +1,150 @@
-let example = 1;
- example1 = example1 + 1;
- example1++;
- example1 += 1;
+// let example = 1;
+//  example1 = example1 + 1;
+//  example1++;
+//  example1 += 1;
 
 
- let example = -1;
- example1 = example1 - 1;
- example1--;
- example1 -= 1;
+//  let example = -1;
+//  example1 = example1 - 1;
+//  example1--;
+//  example1 -= 1;
 
 
- example1 = example1 / 2;
- example1 /=2;
+//  example1 = example1 / 2;
+//  example1 /=2;
 
 
- example1 = example * 2;
- example1 *= 2;
+//  example1 = example * 2;
+//  example1 *= 2;
   
- let example2 = 1 - -1;
- let example2 = -1;
- let example3 = +1;
+//  let example2 = 1 - -1;
+//  let example2 = -1;
+//  let example3 = +1;
 
 
- let example4 = 100 + 5 *3 - -12 / 2 +12 ** (1/2);
+//  let example4 = 100 + 5 *3 - -12 / 2 +12 ** (1/2);
 
- let primer = 1 +"1";
- let primet1 = "1"+1;
+//  let primer = 1 +"1";
+//  let primet1 = "1"+1;
 
- console.log(primer1);
+//  console.log(primer1);
  
- let primer = "-1";
- console.log(+primer9 + 1);
+//  let primer = "-1";
+//  console.log(+primer9 + 1);
 
- let primer10 = +"1";
- let primer11 = Number("123456");
- let primer12 = Number("asedfgv"); //NaN 
- let primer13 = 134 + Math.random() *400;
- let example14 = 3.14595620;
- example14.toFixed(5);
- let primer50 = 7410.8523674;
- let primer45 = primer50.toFixed(2);
-
-
-
- let a = prompt("How many dollars do you have?");
- let b = prompt("What is dollar exchange rate?");
-let c= parseFloat(a)* parseFloat(b);
+//  let primer10 = +"1";
+//  let primer11 = Number("123456");
+//  let primer12 = Number("asedfgv"); //NaN 
+//  let primer13 = 134 + Math.random() *400;
+//  let example14 = 3.14595620;
+//  example14.toFixed(5);
+//  let primer50 = 7410.8523674;
+//  let primer45 = primer50.toFixed(2);
 
 
-  alert("tou have "+ c + "soms!")
+
+//  let a = prompt("How many dollars do you have?");
+//  let b = prompt("What is dollar exchange rate?");
+// let c= parseFloat(a)* parseFloat(b);
+
+
+//   alert("tou have "+ c + "soms!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var boolean1 = true;
+// var boolean2 = false;
+
+// var example1 = 5 < 6;
+// var example2 = 5 > 6;
+// var example3 = 5 == 5;
+// var example4 = 5 != 5;
+// var example5 = 5 <= 5;
+// var example6 = 5 >= 5;
+// var example7 = "Hello" == "World";
+// var example8 = "Hello" != "Hello";
+// var example9 = "A" > "Z";
+
+
+// var example10 = "2" > 1;
+// var example11 = 1 < "007";
+// var example12 = 0 == "0";
+
+// var example13 = Boolean(1); //true
+// var example14 = Boolean(123456);//true
+// var example15 = Boolean(0);//false
+// var  exampl16 = Boolean("Hello");// true
+// let example17 = Boolean(" ");//true
+// let example18 = Boolean("");//false
+// let example19 = Boolean("0")//true
+// let example20 = example19 == example15;
+
+
+// let example21 = 0 == false ;//true
+// let example22 = '' == false;
+// let example23 = 0 === false;
+
+// let example24 = 0 == false ;//false
+// let example25 = '' == false;//false
+// let example26 = 0 === false;//false
+
+
+// let example27 = null ==undefined;//true
+// let example28 = null !== undefined //true
+ 
+
+
+
+
+
+
+// let a = "Hello";
+// if (a === "Hello"){
+//     console.log("A is Hello");
+// }
+// if (a === "World"){
+//     console.log("A is world");
+// }
+// if (!a){
+//     console.log("A!");
+// }
+
+
+
+
+
+
+
+
+let a = prompt("What is A");
+if (a){
+    console.log("A squared eqguals to" + (a ** 2));
+}
+if (!a) {
+console.log("A not entered");
+}
