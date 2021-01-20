dcf83795d5620a25e3dc36d022637ee0f09692aa
Your Name <you@example.com>
2021-01-20 13:48:27 +0600

    added new let prompt

---
+++ script1.js
@@ -0,0 +1,51 @@
+let example = 1;
+ example1 = example1 + 1;
+ example1++;
+ example1 += 1;
+
+
+ let example = -1;
+ example1 = example1 - 1;
+ example1--;
+ example1 -= 1;
+
+
+ example1 = example1 / 2;
+ example1 /=2;
+
+
+ example1 = example * 2;
+ example1 *= 2;
+  
+ let example2 = 1 - -1;
+ let example2 = -1;
+ let example3 = +1;
+
+
+ let example4 = 100 + 5 *3 - -12 / 2 +12 ** (1/2);
+
+ let primer = 1 +"1";
+ let primet1 = "1"+1;
+
+ console.log(primer1);
+ 
+ let primer = "-1";
+ console.log(+primer9 + 1);
+
+ let primer10 = +"1";
+ let primer11 = Number("123456");
+ let primer12 = Number("asedfgv"); //NaN 
+ let primer13 = 134 + Math.random() *400;
+ let example14 = 3.14595620;
+ example14.toFixed(5);
+ let primer50 = 7410.8523674;
+ let primer45 = primer50.toFixed(2);
+
+
+
+ let a = prompt("How many dollars do you have?");
+ let b = prompt("What is dollar exchange rate?");
+  let c= parseFloat(a)* parseFloat(b);
+
+
+  alert("tou have "+ c + "soms!")
