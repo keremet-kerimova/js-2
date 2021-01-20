c8fe7988a94a78cf9a9a50a35f5df32d3c7f5d3e
Your Name <you@example.com>
2021-01-20 17:04:36 +0600

    added five quesions

--- java-homework/java.js
+++ java-homework/java.js
@@ -1,7 +1,36 @@
-let point = 0;
+// let points = 0;
 
-let answer1 = prompt ("What is  the  capital of Kyrgyz pepublic ?");
-if (answer1 === "Bishkek"){
-    points = points + 5;
-}
-console.log( "You've earned " + point + " points");
+// let answer1 = prompt ("What is  the  capital of Kyrgyz pepublic ?");
+// if (answer1 === "Bishkek"){
+//     points = points + 10;
+// }
+
+
+
+// let answer2 = prompt ("How many districts are there  in Kyrgyzstan?");
+// if (answer2 === "41"){
+//     points = points + 10;
+// }
+// alert("You've earned " + points + " points");
+
+
+
+
+
+// let answer3 = prompt ("Hydrogen formula?");
+// if (answer3 === "H2O"){
+//     points = points + 10;
+// }
+//  alert("You've earned " + points + " points");
+
+
+// let answer4 = prompt ("You are love you?");
+// if (answer4 === ""){
+//    points = points + 10;
+//  }
+//  alert("You've earned " + points + " points");
+
+let answer3 = prompt (`Author book "Rich Dad and Poor Dad?"`);
+if (answer3 === "Robert"){
+     points = points + 10;
+ }
