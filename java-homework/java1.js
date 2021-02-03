d00e6ac1ee236d5e3bf9ed606feaac1d69845d9f
Your Name <you@example.com>
2021-02-03 14:29:50 +0600

    aaded new file

--- java-homework/java1.js
+++ java-homework/java1.js
@@ -1,10 +1,21 @@
- let unit = prompt ("Какие нации живут в Кыргызстане?");
- if (unit == "Узбекбы"  || unit == "Дунгане"){
-     console.log ("True")
- }
-else if ( unit == "Русские" || unit == "Таджики"){
-    console.log ("True")
-}
-else {
-    console ("False")
-}
+//  let unit = prompt ("Какие нации живут в Кыргызстане?");
+//  if (unit == "Узбекбы"  || unit == "Дунгане"){
+//      console.log ("True")
+//  }
+// else if ( unit == "Русские" || unit == "Таджики"){
+//     console.log ("True")
+// }
+// else {
+//     console ("False")
+// }
+
+
+// let a = +prompt("What is A?");
+// let b = +prompt("what is B?");
+// document.write(a + b);
+
+
+
+let number = ["one","two", "three", "four","five"]
+number.push = "six";
+document.write(number[2]);
